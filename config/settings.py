f8893e231fda3e90d8ac92c33b87a692d89bbb22
In Kyu Han <sirasonee@gmail.com>
2022-04-20 16:53:55 +0900

    mainpage almost done (features & redirection left)

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
 
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mainpage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +78,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+
+        'NAME': 'recipe',
+
+        'USER': 'root',
+
+        'PASSWORD': '12341234',
+
+        'HOST': '34.71.253.5',
+
+        'PORT': 3306
     }
 }
 
---
+++ mainpage/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from mainpage import views
+
+app_name = 'mainpage'
+urlpatterns = [
+    path('mainpage/', views.main, name='mainpage'),
+]
