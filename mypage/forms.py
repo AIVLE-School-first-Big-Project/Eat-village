2952bc63c546354458865ffc88d3abfbe3d59696
JI HEE <syos7462@daum.net>
2022-04-22 14:27:15 +0900

    jihee - 템플릿

---
+++ mypage/forms.py
@@ -0,0 +1,8 @@
+from msilib.schema import Class
+from django import forms
+from .models import *
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
