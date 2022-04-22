6da9c3350029efc28df6dee223a46b72323fbd04
In Kyu Han <ikhan94305@gmail.com>
2022-04-22 16:37:53 +0900

    Front parts are mostly done

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -6,6 +6,8 @@ app_name = 'mainpage'
 urlpatterns = [
     path('mainpage/', views.main, name='mainpage'),
     path('weather/', views.weather_recomm, name='weather'),
-    path('ingred/', views.ingred_recomm, name='ingredient'),
+    path('ingred_recipe/', views.ingred_recomm, name='ingredient_recom'),
     path('search/', views.recipe_search, name='search'),
+    path('ingred_result/', views.ingred_result, name='ingredient_result'),
+    path('ingred_change/', views.ingred_change, name='ingredient_change'),
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -10,7 +10,13 @@ def weather_recomm(request):
     return render(request, 'mainpage/weather_recom.html')
 
 def ingred_recomm(request):
-    return render(request, 'mainpage/recipe_recomm.html')
+    return render(request, 'mainpage/recipe_recom.html')
 
 def recipe_search(request):
     return render(request, 'mainpage/recipe_search.html')
+
+def ingred_result(request):
+    return render(request, 'mainpage/ingredients_result.html')
+
+def ingred_change(request):
+    return render(request, 'mainpage/ingredients_change.html')
