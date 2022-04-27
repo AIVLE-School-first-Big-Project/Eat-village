b15122aee835af56d8fd1737690c867d26d3cb13
yeon <mamen7624@gmail.com>
2022-04-27 11:26:54 +0900

    수정

--- users/models.py
+++ users/models.py
@@ -1,13 +1,9 @@
+from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import AbstractUser	# AbstractUser 불러오기
 
 class User(AbstractUser):
     test_list = models.CharField(max_length=20, default="") #알레르기체크박스
-    ale = models.CharField(max_length=20, null=True) #알레르기
-    postcode = models.CharField(max_length=50, null=True) # 우편번호
     address = models.CharField(max_length=50, null=True) # 주소
-    detailAddress = models.CharField(max_length=100, null=True) #상세주소
-    extraAddress = models.CharField(max_length=50, null=True) #참고항목
-    test1 = models.CharField(max_length=50, null=True)
-
+    confirm_password = models.CharField(max_length=20)
                      
