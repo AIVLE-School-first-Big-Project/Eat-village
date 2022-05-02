00652d657ee13146bf4d903e710c9ebd040519f3
JI HEE <syos7462@daum.net>
2022-05-02 14:54:36 +0900

    mypage, 탈퇴화면, 글 목록 :css 수정

--- mypage/urls.py
+++ mypage/urls.py
@@ -5,7 +5,7 @@ from .views import *
 app_name = 'mypage'
 
 urlpatterns = [
-    path('main/', views.user_info),
+    path('main/', views.user_info, name='main'),
     path('test/', views.home, name='write'),
     # path('userDelete/', views.user_delete, name='profile_delete'),
     path('write_list/', views.show_writeList, name='write_list')
