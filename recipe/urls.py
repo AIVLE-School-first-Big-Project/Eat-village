ca22356e9837de3bcb9d80374cb8e88c9a5dbfd5
chanho <joungch2015@naver.com>
2022-05-11 08:33:31 +0900

    .

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     # path('main/',views.main),
-    path('insert/',views.insert, name='insert'),
-    # path('show/',views.show, name='show' ),
-    path('recommend/',views.recommend, name='recommend'),
-    path('video_feed/',views.video_feed, name='video_feed'),
-    # path('test/',views.test, name='test'),
+    # path('insert/',views.insert, name='insert'),
+    # # path('show/',views.show, name='show' ),
+    # path('recommend/',views.recommend, name='recommend'),
+    # path('video_feed/',views.video_feed, name='video_feed'),
+    # # path('test/',views.test, name='test'),
 ]
